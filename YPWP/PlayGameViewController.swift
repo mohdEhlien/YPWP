a736a2afabbbe27d2e949c03a1fd2157cc70b318
Mohamed <mohamed.mohd@hotmail.com>
2015-11-04 18:41:33 -0500

    Hello

--- YPWP/PlayGameViewController.swift
+++ YPWP/PlayGameViewController.swift
@@ -16,21 +16,30 @@ class PlayGameViewController: UIViewController {
 	@IBOutlet weak var answerTF: UITextField!
 	@IBOutlet weak var skipItButton: UIButton!
 	@IBOutlet weak var answerItButton: UIButton!
+	@IBOutlet weak var wrongLabel: UILabel!
+	@IBOutlet weak var correctLabel: UILabel!
+	@IBOutlet weak var skippedLabel: UILabel!
+	@IBOutlet weak var timerLabel: UILabel!
+	
 	
 	let colorWheel = ColorWheel()
 	
-	var question : String! = ""
+	var questionQ : String! = ""
+	var questionL : String! = ""
 	var answer : String! = ""
 	var objectIDArray = [String]()
 	var randomID = 0
 	var rightAnswers = 0
 	var wrongAnswers = 0
 	var skippedQuestions = 0
+	var timer = NSTimer()
+	var counter = 20
 	
 	@IBAction func skipItButton(sender: AnyObject) {
 		//self.alert("Opps!", textMessage: "You just skipped a question. This will not be added to your pool.")
 		answerTF.text = ""
-		print("Skipped Questions: \(++skippedQuestions)")
+		counter = 20
+		skippedLabel.text = "Skipped: \(++skippedQuestions)"
 		self.questionLabel.alpha = 0
 		changeColor()
 		callData()
@@ -39,16 +48,18 @@ class PlayGameViewController: UIViewController {
 	@IBAction func acceptItButton(sender: AnyObject) {
 		if ((answerTF.text?.caseInsensitiveCompare(self.answer)) == NSComparisonResult.OrderedSame) {
 			answerTF.text = ""
-			print("Right Answers: \(++rightAnswers)")
+			counter = 20
+			correctLabel.text = "Correct: \(++rightAnswers)"
 			self.questionLabel.alpha = 0
 			changeColor()
 			removeAndUpdateQuestion()
 		} else if ( answerTF.text == "" ) {
 			alert("Empty Field", textMessage: "Looks like you forgot to put the answer. Either press next or skip the question.")
 		} else {
-			alert("Oh oh.", textMessage: "Sorry that was the wrong answer...")
+			alert("Oh oh.", textMessage: "Sorry that was the wrong answer... Don't worry, you'll get another chance at it next time!")
 			answerTF.text = ""
-			print("Wrong Answers: \(++wrongAnswers)")
+			counter = 20
+			wrongLabel.text = "Wrong: \(++wrongAnswers)"
 			self.questionLabel.alpha = 0
 			changeColor()
 			callData()
@@ -63,23 +74,29 @@ class PlayGameViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.view.endEditing(true)
-		changeColor()
 		
-		// FIND THE OBJECT IDs FOR THE QUESTIONS
-		let objectIdQuery : PFQuery = PFQuery(className: "QuestionsAndAnswers")
+		changeColor()
+		resetScoresToZero()
+		
+		self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("startTimer:"), userInfo: nil, repeats: true)
+		self.timerLabel.text = "Question will change in: \(counter)s"
+		
+		let objectIdQuery : PFQuery = PFQuery(className: "GeographyQA")
 		objectIdQuery.findObjectsInBackgroundWithBlock {
 			(objects : [PFObject]? , error : NSError?) -> Void in
 			
 			var objectID = objects! as [PFObject]
+			
 			for i in 0..<objectID.count {
 				self.objectIDArray.append(objectID[i].objectId!)
 			}
+			
 			self.callData()
 		}
-        // Do any additional setup after loading the view.
 	}
 	
 	override func viewWillAppear(animated: Bool) {
+		self.becomeFirstResponder()
 		self.navigationController!.navigationBar.hidden = false
 		self.navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
 		self.navigationController!.navigationBar.shadowImage = UIImage()
@@ -90,24 +107,23 @@ class PlayGameViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
 	}
 	
 	func getRandomObjectID() {
-		// RANDOMIZE THE QUESTIONS USING THE OBJECT ID
 		randomID = Int(arc4random_uniform(UInt32(objectIDArray.count)))
 	}
 	
 	func callData() {
 		getRandomObjectID()
 		if ( objectIDArray.count > 0 ) {
-		let query : PFQuery = PFQuery(className: "QuestionsAndAnswers")
+		let query : PFQuery = PFQuery(className: "GeographyQA")
 		query.getObjectInBackgroundWithId(objectIDArray[randomID]) {
 			(object : PFObject? , error : NSError?) -> Void in
 			if error == nil {
-				self.question = object!["Question"] as! String!
+				self.questionQ = object!["QuestionQ"] as! String!
+				self.questionL = object!["QuestionL"] as! String!
 				self.answer = object!["Answer"] as! String!
-				self.questionLabel.text = self.question
+				self.questionLabel.text = self.questionQ + " " + self.questionL
 				self.questionLabel.alpha = 0
 				UIView.animateWithDuration(1.0, animations: {
 					self.questionLabel.alpha = 1
@@ -142,15 +158,22 @@ class PlayGameViewController: UIViewController {
 		alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
 		self.presentViewController(alertController, animated: true, completion: nil)
 	}
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	
+	func resetScoresToZero() {
+		skippedLabel.text = "Skipped: 0"
+		correctLabel.text = "Correct: 0"
+		wrongLabel.text = "Wrong: 0"
+	}
+	
+	func startTimer(timer: NSTimer) {
+		
+		self.timerLabel.text = "Question will change in: \(counter--)s"
+		if counter == -1 {
+			skippedLabel.text = "Skipped: \(++skippedQuestions)"
+			counter = 20
+			callData()
+		}
+	}
+	
 
 }
